92817b0fe8dd1cfd9744a4f0e42cb485c6f7b680
Tom <gilmore.tom@gmail.com>
2025-04-07 14:37:16 -0400

    New4

--- map.js
+++ map.js
@@ -1,3 +1,9 @@
 'use strict'
 console.log('Loaded map.js')
 mapboxgl.accessToken = pk.eyJ1IjoidGhvbWdpbDQ3IiwiYSI6ImNtOTdlajVveDA2bXgyb3BzMjJmdWwxMnEifQ.XaAyS7abvzpEbYM0ZcKGSw
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-73.93324, 40.80877],
+    zoom: 14
+});
